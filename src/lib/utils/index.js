a5d74a869936355a23e7d6d642f47c273a89a45f
anesterenko <anastasiya.nesterenko@techvice.org>
2021-01-15 15:49:03 +0300

    feat: change arrows in ma crossower

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -16,6 +16,7 @@ export { default as shallowEqual } from "./shallowEqual";
 export { default as mappedSlidingWindow } from "./mappedSlidingWindow";
 export { default as accumulatingWindow } from "./accumulatingWindow";
 export { default as PureComponent } from "./PureComponent";
+export { default as uniqueId } from "./uniqueId";
 
 export * from "./barWidth";
 export * from "./strokeDasharray";
